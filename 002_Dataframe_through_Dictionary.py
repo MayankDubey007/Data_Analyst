e5af79de7e2769afdef4ecadf26a109adf2988ba
MayankDubey007 <81865238+MayankDubey007@users.noreply.github.com>
2023-09-20 18:50:12 +0530

    Create 002_Dataframe_through_Dictionary.py

---
+++ 002_Dataframe_through_Dictionary.py
@@ -0,0 +1,8 @@
+# Store the data in a dictionary
+employee_dict = {'Employee Name':['Rajeev', 'Sumit', 'Aviral'], 'Income':[200000, 140000, 90000]}
+
+# Create DataFrame
+employee_df = pd.DataFrame(employee_dict)
+
+# Print the dataframe
+employee_df
